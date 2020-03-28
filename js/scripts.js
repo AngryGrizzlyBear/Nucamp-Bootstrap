81dfe7da4853615a68da9ed27e8297b6d802713b
Evan Longwood <evanmlongwood@gmail.com>
2020-03-28 11:06:54 -0700

    week 4 assignment.

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,12 @@ $(function () {
         }
     });
 });
+
+$(function () {
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("show");
+    });
+    $("#loginButton").click(function () {
+        $("#loginModal").modal("show");
+    });
+});
